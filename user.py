b31c1ba30552bc70c7a0b8de5c0ed501176ab84f
Gabriel Nii Attoh Quaye <127400736+links-astec@users.noreply.github.com>
2024-02-16 13:57:28 +0000

    this is the solution to today's assignment
    the fizz.py is the solution to the fizzbuzz assignment and the user.py is the solution to the username assignment
    my name is Gabriel Nii Attoh Quaye with index number 1827522
    and a friend I worked with who couldn't use his pc is Gabriel Kwankye Yeboah with index number 1830022

---
+++ user.py
@@ -0,0 +1,9 @@
+users=['ama','kojo','gyamerah','simon']
+new_users=['adjoa','kojo','kofi','simon','frank']
+
+for user in new_users and  new_users:
+    if user in users and new_users:
+        print(f'the username {user} is already taken type a new username')
+    else:
+        print(f'this username {user} is available')
+
